dc6ea52b9774a65c1d0b60ced008097dd82b615d
Katherine Whitlock <toroidalcode@gmail.com>
2013-05-16 17:42:16 -0400

    GADDAG

---
+++ src/java/GADDAG.java
@@ -0,0 +1,81 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class GADDAG extends Trie {
+    private static char separator = '>';
+
+    public GADDAG(){ root = new Node(Node.root); }
+
+    @Override
+    public void add(String word){
+        if (word.length() == 0) return;
+
+        word = word.toLowerCase();
+
+        String prefix;
+        char[] ch;
+        int i;
+        for (i = 1; i < word.length(); i++){
+            prefix = word.substring(0,i);
+            ch = prefix.toCharArray();
+            reverse(ch);
+            super.add(new String(ch) + separator + word.substring(i));
+        }
+        ch = word.toCharArray();
+        reverse(ch);
+        super.add(new String(ch) + separator + word.substring(i));
+    }
+
+    private void reverse(char[] validData){
+        for(int i = 0; i < validData.length/2; i++)
+        {
+            int temp = validData[i];
+            validData[i] = validData[validData.length - i - 1];
+            validData[validData.length - i - 1] = (char)temp;
+        }
+    }
+
+    public HashSet<String> findWordsWithRackAndHook(Character[] rack, char hook){
+        HashSet<String> words = new HashSet<String>();
+        Arrays.sort(rack);
+        ArrayList<Character> rackList = new ArrayList<Character>(Arrays.asList(rack));
+
+        if (hook == ' '){
+            char tile;
+            while (rackList.size() > 1){
+                tile = rackList.remove(0);
+                findWordsRecurse(words, "",  rackList, tile, root, true);
+            }
+        } else {
+            findWordsRecurse(words, "", rackList,  hook, root, true);
+        }
+        return words;
+    }
+
+    private void findWordsRecurse(HashSet<String> words, String word, ArrayList<Character> rack, char hook, Node cur, boolean direction){
+        Node hookNode = cur.getChild(hook);
+
+        //Base case
+        if (hookNode == null)
+            return;
+
+        String hookCh = hook == separator ? "" : String.valueOf(hook); //Empty character if we're the separator
+        word = (direction ? hookCh + word : word + hookCh); //Direction-based concatenation
+
+        //if we've reached the end a word, add the word to output
+        if (hookNode.getFinite())
+            words.add(word);
+
+        for (char nodeKey : hookNode.getKeys()) {
+            if (nodeKey == separator)
+                findWordsRecurse(words, word, rack, separator, hookNode, false);
+            else if (rack.contains(nodeKey)){
+                //boolean duplicate = (rack.size() > 0 && (rack.get(nodeKey) == rack.get(rack.indexOf(nodeKey) - 1)));
+                ArrayList<Character> newRack = (ArrayList<Character>) rack.clone();
+                newRack.remove((Character)nodeKey);
+                findWordsRecurse(words, word, newRack, nodeKey, hookNode, direction);
+            }
+        }
+    }
+}
---
+++ src/java/Test.java
@@ -0,0 +1,57 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Test {
+
+    public static Character[] toCharacterArray(String s) {
+        if (s == null) {
+            return null;
+        }
+        Character[] array = new Character[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            array[i] = s.charAt(i);
+        }
+        return array;
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1){
+            System.out.println("Please enter a dictionary file when executing");
+            System.exit(-1);
+        }
+
+        GADDAG g = new GADDAG(); // change to trie to see a trie.
+        //g.addAll(new String[]{"Abstract", "Absolute", "Absolve", "Butts", "Boats", "Bannana", "Abs"});
+        long startTime = System.nanoTime();
+
+        Scanner dictionary = new Scanner(new File(args[0]));
+        while(dictionary.hasNext()) {
+            g.add(dictionary.nextLine());
+        }
+
+        long duration = System.nanoTime() - startTime;
+        System.out.println("Time to build GADDAG: " + duration/1000000000. + " seconds");
+        repl(g);
+    }
+
+    public static void repl(GADDAG g){
+        System.out.println("Welcome to the word finder");
+        System.out.println("Enter in 'r', followed by the rack you want to use to search.");
+        System.out.println("Use 'h' followed by a character to change the hook.");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("> ");
+        char hook = ' ';
+        while(scan.hasNext()){
+            String nextLine = scan.next();
+            if (nextLine.equals("r")){
+                String rack = scan.next();
+                System.out.println(g.findWordsWithRackAndHook(toCharacterArray(rack.toUpperCase()), hook));
+            }
+            else if (nextLine.equals("h")){
+                hook = scan.next().toUpperCase().charAt(0);
+                System.out.println("Hook is now: " + hook);
+            }
+            System.out.print("> ");
+        }
+    }
+}
