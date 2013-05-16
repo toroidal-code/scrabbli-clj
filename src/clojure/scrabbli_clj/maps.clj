4de870fd16fb166111c67fe3d36aff35d5325e19
Zachary Elliott <zach@nyu.edu>
2013-05-16 17:59:05 -0400

    Renaming constants to fall within expected lisp naming conventions

--- src/clojure/scrabbli_clj/maps.clj
+++ src/clojure/scrabbli_clj/maps.clj
@@ -1,3 +1,4 @@
+
 (ns scrabbli-clj.maps)
 
 ; 0 points: blank *2
@@ -12,19 +13,21 @@
 ; 100 total tiles
 ; 187 total points
 
-(def SCORES (hash-map :a 1 :b 3 :c 3 :d 2 :e 1 :f 4 :g 2 :h  4 :i 1
-                      :j 8 :k 5 :l 1 :m 3 :n 1 :o 1 :p 3 :q 10 :r 1
-                      :s 1 :t 1 :u 1 :v 4 :w 4 :x 8 :y 4 :z 10 :_ 0))
+(def ^:dynamic *scores*
+  (hash-map :a 1 :b 3 :c 3 :d 2 :e 1 :f 4 :g 2 :h 4 :i 1
+            :j 8 :k 5 :l 1 :m 3 :n 1 :o 1 :p 3 :q 10 :r 1
+            :s 1 :t 1 :u 1 :v 4 :w 4 :x 8 :y 4 :z 10 :_ 0))
 
-(def TILES  (hash-map :a 9 :b 2 :c 2 :d 4 :e 12 :f 2 :g 3 :h 2 :i 9
-                      :j 1 :k 1 :l 4 :m 2 :n  6 :o 8 :p 2 :q 1 :r 6
-                      :s 4 :t 6 :u 4 :v 2 :w  2 :x 1 :y 2 :z 1 :_ 2))
+(def ^:dynmaic *tiles*
+  (hash-map :a 9 :b 2 :c 2 :d 4 :e 12 :f 2 :g 3 :h 2 :i 9
+            :j 1 :k 1 :l 4 :m 2 :n  6 :o 8 :p 2 :q 1 :r 6
+            :s 4 :t 6 :u 4 :v 2 :w  2 :x 1 :y 2 :z 1 :_ 2))
 
 ;; T triple word
 ;; t triple letter
 ;; D double word
 ;; d double letter
-(def BOARD
+(def ^:dynamic *board*
   [[  :T "." "."  :d "." "." "."  :T "." "." "."  :d "." "."  :T]
    [ "."  :D "." "." "."  :t "." "." "."  :t "." "." "."  :D "."]
    [ "." "."  :D "." "." "."  :d "."  :d "." "." "."  :D "." "."]
